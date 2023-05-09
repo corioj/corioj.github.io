67bf5e29fea4c480e5498efa7e9d9783c7cd1182
corioj <corioj@umich.edu>
2023-05-09 15:39:29 -0400

    updated with spring resume

--- resumeJS/download.js
+++ resumeJS/download.js
@@ -9,5 +9,5 @@ function download(url) {
 
 const downloadZone = document.getElementsByClassName("download-resume");
 downloadZone[0].addEventListener("click", function() {
-	download("resumeJS/John_Corio_ResumeF22.pdf")
+	download("resumeJS/CorioJ_DS_Resume_SP23.pdf")
 });
