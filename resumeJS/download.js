e665a3d4f1787c5f2429925bc6c48685f361ae28
John Corio <44953944+corioj@users.noreply.github.com>
2025-04-04 20:58:24 -0400

    changed resume out
    resume 1

--- resumeJS/download.js
+++ resumeJS/download.js
@@ -9,5 +9,5 @@ function download(url) {
 
 const downloadZone = document.getElementsByClassName("download-resume");
 downloadZone[0].addEventListener("click", function() {
-	download("resumeJS/CorioJohn_DS_CV.pdf")
+	download("resumeJS/JCorio_GDev_CV.pdf")
 });
